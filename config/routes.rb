8921b680f5b4bdf0d276151d0e85e5f8bc7808b3
Mike Allred <mikejallred@gmail.com>
2018-04-17 14:56:25 -0400

    Api namespace, fixed model.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :inventories
-  resources :products
+  scope '/api' do
+    resources :inventories
+    resources :products
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
